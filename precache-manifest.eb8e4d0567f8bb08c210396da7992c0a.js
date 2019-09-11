10cc28b8435d96909678b24715c28dad8c12a4f5
BRKsReginaldo <BRKsReginaldo@users.noreply.github.com>
2019-09-11 12:52:55 +0000

    Deploying to master - 12:52:55

---
+++ precache-manifest.eb8e4d0567f8bb08c210396da7992c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346305dc6ea7e73ba0231b1ffa798704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "728d7f5157e0ba4c0008",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f253c359e1015e5ebe2d",
+    "url": "/static/js/2.3aeacd8d.chunk.js"
+  },
+  {
+    "revision": "728d7f5157e0ba4c0008",
+    "url": "/static/js/main.27aa0a58.chunk.js"
+  },
+  {
+    "revision": "6c5a564e24cf20d7a12a",
+    "url": "/static/js/runtime~main.fd9d6295.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
